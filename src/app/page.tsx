79b564ecdf781f78149efe528ea0a99222808476
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2025-11-04 15:54:28 +0900

    feat: iconを変更

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import AboutPage from "./about"
+import "./styles.css"
+
+export default function rootLayout() {
+  return (
+    <html>
+      <body>
+        <AboutPage />
+      </body>
+    </html>
+  )
+}
